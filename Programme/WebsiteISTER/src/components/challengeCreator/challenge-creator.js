3e4e3e030ff269a2ae2cd7887da2733f93832e68
mazinator <daniel.maz99@gmail.com>
2018-10-29 18:52:00 +0100

    not worth describing

--- Programme/WebsiteISTER/src/components/challengeCreator/challenge-creator.js
+++ Programme/WebsiteISTER/src/components/challengeCreator/challenge-creator.js
@@ -10,11 +10,52 @@ class ChallengeCreator extends LitElement{
         super();
     }
 
+    createNewChallenge(){
+        console.log('entered createNewChallenge')
+    }
+
     render(){
         return html`
         <link rel="stylesheet" type="text/css" href=/src/components/challengeCreator/styles.css>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <div class="mainPos">
-            <h1>asdfasdf</h1>
+            <h1>Create a new Challenge</h1><br>
+            <input data-provide="datepicker">
+            <input class="datepicker" data-date-format="mm/dd/yyyy">
+            <div class="dropdown">
+                <form>
+                    <div class="form-group">
+                        <p>Year</p>
+                        <input id="year" type="text" placeholder="year start">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <p>1. Session</p>
+                        <select id="dropDownResult" class="form-control">
+                            <option value="0">Total</option>
+                            <option value="1">1st Session</option>
+                            <option value="2">2nd Session</option>
+                            <option value="3">3rd Session</option>
+                            <option value="4">4th Session</option>
+                            <option value="5">5th Session</option>
+                            <option value="6">6th Session</option>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <p>Sequence</p>
+                        <select id="dropDownSequence" class="form-control">
+                            <option value="Alphabetic">Alphabetic</option>
+                            <option value="TopDown">TopDown</option>
+                            <option value="Categories">Categories</option>
+                        </select>
+                    </div>
+                </form>
+			</div>
+            <br>
+            <input type ="button" value="Create Challenge" class="btn btn-primary" @click="${() => this.createNewChallenge()}"></input>
         </div>
         `
     }
