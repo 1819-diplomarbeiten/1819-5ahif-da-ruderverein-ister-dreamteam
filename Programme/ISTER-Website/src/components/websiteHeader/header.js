cec0b10523f4aa289ad96ea6271993a0e79683f4
mazinator <daniel.maz99@gmail.com>
2018-11-12 20:02:10 +0100

    created website countdown

--- Programme/ISTER-Website/src/components/websiteHeader/header.js
+++ Programme/ISTER-Website/src/components/websiteHeader/header.js
@@ -9,11 +9,39 @@ class WebSiteHeader extends LitElement{
         console.log(language + ' selected as language')
     }
 
+    getCountdownStanding(){
+        var countDownDate = new Date("Dec 5, 2018 15:37:25")
+        return {
+            "time": countDownDate,
+            "state": "STARTS"
+        }
+    }
+
+    countdown(){
+        var countDownDate = this.getCountdownStanding();
+
+        var x = setInterval(() => {
+
+            var distance = countDownDate.time.getTime() - new Date().getTime();
+            
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+            this.shadowRoot.getElementById('demo').innerHTML = 'THE CHALLENGE ' + countDownDate.state + ' ==> ' + days + ' Days ' + hours + ' Hours ' + minutes + ' Minutes ' + seconds + ' Seconds'
+        }, 1000);
+    }
+
     render(){
+        window.onload = () => {
+            this.countdown()
+        }
         return html`
             <div class="background">
                 <link rel="stylesheet" type="text/css" href=/src/components/websiteHeader/styles.css>
-                <h1 class="header">THE CHALLENGE STARTS ==>  34 Days 05 Hours 48 Minutes 26 Seconds</h1>
+                <h1 class="header" id="demo" @click="${() => this.countdown()}"></h1>
                 <div class="languages">
                     <img src="images/germanFlag.png" width="70" height="35" @click="${() => this.changeLanguage('german')}">
                     <img src="images/englishFlag.png" width="70" height="35" @click="${() => this.changeLanguage('english')}">
