b781f46742528cad0923d5739a315f1db20a663d
mazinator <daniel.maz99@gmail.com>
2018-12-24 12:26:57 +0100

    more stuff

--- Programme/ISTER-Website/src/components/overviewContent/loginForm/login.js
+++ Programme/ISTER-Website/src/components/overviewContent/loginForm/login.js
@@ -11,10 +11,22 @@ export default class LoginForm extends LitElement{
 
     render(){
         return html`
+        <script lang="javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script lang="javascript" src="/node_modules/jquery/dist/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="/src/components/overviewContent/loginForm/styles.css"></link>
         <div class="mainPos">
             <p id="testId">I am a Login Form</p>
             <button @click="${() => this.testMethod('testId')}">Click me</button>
+            <br>
+            <div id="schoenVerpackt">
+                <div class="input-group input-group-sm mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-sm"><strong>Distance</strong></span>
+                    </div>
+                    <input id="distance" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
         </div>
         `
     }
