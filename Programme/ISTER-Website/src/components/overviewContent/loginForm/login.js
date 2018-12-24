194c12d5f01627f50aaeb68e0adfdbf94521262c
mazinator <daniel.maz99@gmail.com>
2018-12-24 12:19:57 +0100

    stuff

--- Programme/ISTER-Website/src/components/overviewContent/loginForm/login.js
+++ Programme/ISTER-Website/src/components/overviewContent/loginForm/login.js
@@ -5,11 +5,16 @@ export default class LoginForm extends LitElement{
         super();
     }
 
+    testMethod(paramId){
+        console.log('content of p: ' + this.shadowRoot.getElementById(paramId).innerHTML)
+    }
+
     render(){
         return html`
         <link rel="stylesheet" type="text/css" href="/src/components/overviewContent/loginForm/styles.css"></link>
         <div class="mainPos">
-            <p>I am a Login Form</p>
+            <p id="testId">I am a Login Form</p>
+            <button @click="${() => this.testMethod('testId')}">Click me</button>
         </div>
         `
     }
