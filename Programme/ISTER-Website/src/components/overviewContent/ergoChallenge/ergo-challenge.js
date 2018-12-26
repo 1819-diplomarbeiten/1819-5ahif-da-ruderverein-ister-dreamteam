50480e2dd7cc9085349adcea2891564ffc39a552
mazinator <daniel.maz99@gmail.com>
2018-12-26 15:16:30 +0100

    translation for last component

--- Programme/ISTER-Website/src/components/overviewContent/ergoChallenge/ergo-challenge.js
+++ Programme/ISTER-Website/src/components/overviewContent/ergoChallenge/ergo-challenge.js
@@ -1,14 +1,17 @@
 import {LitElement, html} from '@polymer/lit-element'
+import TranslationService from '../../../services/translation/translationService.js'
 
 export default class ErgoChallenge extends LitElement{
     static get properties(){
         return{
             counter: Number,
-            methodEntered: Boolean
+            methodEntered: Boolean,
+            translation: []
         }
     }
     constructor(){
         super();
+        this.translation = TranslationService.getTranslation('ergo-challenge')
     }
 
     getChallengeSessions(){
@@ -29,7 +32,8 @@ export default class ErgoChallenge extends LitElement{
 
     appendChildTo(to){
         var li = document.createElement('li')
-        li.innerHTML = this.counter + '. Session: ' + this.prepareDate(to)
+        //li.innerHTML = this.counter + '. Session: ' + this.prepareDate(to)
+        li.innerHTML = `${this.counter}. ${this.translation["session"]}: ${this.prepareDate(to)}`
         this.counter++
         this.shadowRoot.getElementById('sessionList').appendChild(li)
     }
@@ -51,36 +55,39 @@ export default class ErgoChallenge extends LitElement{
             <link rel="stylesheet" type="text/css" href="/src/components/overviewContent/ergoChallenge/styles.css"></link>
             <div class="mainPos">
                 <div style="margin-left:2%">
-                    <h1><strong>30 (dirty) K Ergo Challenge</strong></h1>
-                    
-                    <h2>Allgmeines:</em></h2>
-                    <p>Bei diesem Bewerb gibt es weder etwas zu gewinnen, noch erhält man eine Medaille.  Alle Ruderer, weiblich</p>
-                    <p>und männlich aus Vereinen oder privat, sind startberechtigt und als Administrator vertrauen wir auf die Richtigkeit</p>
-                    <p>der gelieferten Daten.</p>
-                    <h2>Bewerb:</h2>
-                    <p>Die Teilnehmer sollen versuchen in der Zeit von 30 Minuten so viele Meter am Ergometer zurück zu legen, als</p>
-                    <p>es ihnen möglich ist.  Es gibt über den Winter verteilt sechs (6) Termine, wobei die vier (4) besten Ergebnisse</p>
-                    <p>gewertet werden. Die Termine sind immer von Donnerstag 18:00 Uhr bis Montag 18:00 Uhr berechnet:</p>
-                    <ul id="sessionList">
-                    </ul>                    
-                    <img src="images/ergo_challenge.jpg" width="650" height="433" style="margin-left:50%;margin-top:-30%">
-                    <p>Des weiteren gibt es am 27.01.2019 die Austrian Indoor Rowing Championships</p>
-                    <h2>Ziel:</h2>
-                    <p>Bei vier Termine zusammengerechnet, mindestens 30 000 Meter (Männer) oder 26 000 (Frauen) zu rudern und dabei mit Ruderkollegen aus Österreich und der Welt messen.</p>
-                        <h2>Daten:</h2>
-                        <p>Um an der Challenge teilnehmen zu können, muss man sich mit einer E-Mail auf dieser Website registrieren und weitere persönliche Daten wie Name, Alter, Gewicht, etc. für die Auswertung der Challenge angeben (Das Gewicht dient lediglich zur Berechnung und scheint nicht in der Auswertung auf). Ihre Daten werden vertraulich gespeichert und nicht an Dritte weitergegeben. Bei Fragen wenden Sie sich an challenge@ister.at. Die Reihung erfolgt gemäß der Concept2 Alterseinteilung:</p>
-                        <ul>
-                            <li>SchülerInnen: bis 14 Jahre</li>
-                            <li>JuniorenInnen: 15 – 16 Jahre</li>
-                            <li>JuniorenInnen: 17 – 18 Jahre</li>
-                            <li>Allgemeine Klasse: 19 – 29 Jahre</li>
-                            <li>Masters A: 30 – 39 Jahre</li>
-                            <li>Masters B: 40 – 49 Jahre</li>
-                            <li>Masters C: 50 – 59 Jahre</li>
-                            <li>Masters D: 60 – 69 Jahre</li>
-                            <li>Masters E: 70 – 79 Jahre</li>
-                            <li>Masters F: 80 – 89 Jahre</li>
-                        </ul>
+                        <h1><strong>${this.translation["ergoHeadline"]}</strong></h1>
+                        <div class="body-container">
+                            <img src="images/ergo_challenge.jpg" width="650" height="433" class="image">
+                            <div class="text-container">
+                                <h2>${this.translation["ergoSubheadlineOne"]}</h2>
+                                <p class="lineSeparator">${this.translation["ergoTextOne"]}</p>  
+                                <br>
+                                <h2>${this.translation["ergoSubheadlineTwo"]}</h2>
+                                <p class="lineSeparator">${this.translation["ergoTextTwo"]}</p>
+                                <ul id="sessionList">
+                                </ul>     
+                                <br>
+                                <p class="lineSeparator">${this.translation["ergoTextThree"]}</p>
+                                <br>
+                                <h2>${this.translation["ergoSubheadlineThree"]}</h2>
+                                <p class="lineSeparator">${this.translation["ergoTextThree"]}</p>
+                                <br>
+                                <h2>${this.translation["ergoSubheadlineFour"]}</h2>
+                                <p class="lineSeparator">${this.translation["ergoTextFour"]}</p>
+                                <ul>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[0]}</li>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[1]}</li>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[2]}</li>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[3]}</li>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[4]}</li>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[5]}</li>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[6]}</li>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[7]}</li>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[8]}</li>
+                                    <li>${this.translation["ergoTextFourClasslist"].split(';')[9]}</li>
+                                </ul>
+                            </div>
+                        </div>
                 </div>
             </div>
         `
