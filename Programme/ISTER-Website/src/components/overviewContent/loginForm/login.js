51217544c6ab5dc840d7c5ecb35e1720872f0565
Grisdoff <d.pointner@students.htl-leonding.ac.at>
2019-01-10 11:07:43 +0100

    Added Google-Signin + gitignore changes

--- Programme/ISTER-Website/src/components/overviewContent/loginForm/login.js
+++ Programme/ISTER-Website/src/components/overviewContent/loginForm/login.js
@@ -1,34 +1,44 @@
-import {LitElement, html} from '@polymer/lit-element'
+import {LitElement, html, eventOptions} from '@polymer/lit-element'
+
 
 export default class LoginForm extends LitElement{
+    
     constructor(){
         super();
     }
+
+    ready(){
+        super.ready()
+        this.addEventListener('on-google-signin-success', this._handleSignInEvent)
+    }
+
+        
 
     testMethod(paramId){
         console.log('content of p: ' + this.shadowRoot.getElementById(paramId).innerHTML)
     }
 
+    ezSkinz(){
+        console.log("HANSI")
+        console.log(gapi.auth2.getAuthInstance()['currentUser'].get())
+    }
+
     render(){
         return html`
-        <script lang="javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-        <script lang="javascript" src="/node_modules/jquery/dist/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="/src/components/overviewContent/loginForm/styles.css"></link>
-        <div class="mainPos">
-            <p id="testId">I am a Login Form</p>
-            <button @click="${() => this.testMethod('testId')}">Click me</button>
-            <br>
-            <div id="schoenVerpackt">
-                <div class="input-group input-group-sm mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroup-sizing-sm"><strong>Distance</strong></span>
-                    </div>
-                    <input id="distance" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
-                </div>
-            </div>
-        </div>
+        <head>
+        <link rel="import" href="../../../../bower_components/google-signin/google-signin.html">
+        </head>
+        <google-signin id="signinBtn" client-id="863083094018-90dqbb2kvkiaog6tugmd5gagr7kgf483.apps.googleusercontent.com" @google-signin-success="${(e) => this._handleSignInEvent(e)}"></google-signin>
         `
+    }
+
+    _handleSignInEvent(e){
+        console.log('iaiaiaiaaio')
+        console.log('type', e.type, e)
+        console.log(gapi.auth2.getAuthInstance()['currentUser'].get())
+        this.eventName = e.type
+        this.eventDetail = JSON.stringify(e.detail)
+
     }
 }
 window.customElements.define('login-form', LoginForm)
